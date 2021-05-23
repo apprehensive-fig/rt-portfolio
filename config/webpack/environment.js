87b27aeca335ed262c12549662ece6fa64279f69
alexis sae an <alexis.sae.an@gmail.com>
2021-05-23 13:50:54 +0900

    removed syntax

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,9 +2,6 @@ const { environment } = require('@rails/webpacker')
 
 module.exports = environment
 
-const { environment } = require('@rails/webpacker')
-
-// Bootstrap 4 has a dependency over jQuery & Popper.js:
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
